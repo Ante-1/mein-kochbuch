506ad7ee3760eca8f1c59bd6ecd209ed548b8b61
Ante Schwender <ante.schwender@gmail.com>
2022-07-24 02:40:41 +0200

    write basic queries and mutations update prisma schema

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,13 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
+import { recipesAndIngredientsRouter } from "./recipesAndIngredients";
 import { authRouter } from "./auth";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("auth.", authRouter);
+  .merge("example.", recipesAndIngredientsRouter)
+// .merge("auth.", authRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
---
+++ src/server/router/recipesAndIngredients.ts
@@ -0,0 +1,63 @@
+import { createRouter } from "./context";
+import { z } from "zod";
+
+export const recipesAndIngredientsRouter = createRouter()
+  .query("recipes", {
+    async resolve({ ctx }) {
+      return {
+        recipes: await ctx.prisma.recipe.findMany(),
+      };
+    },
+  })
+  .query("recipe", {
+    input: z.object({ id: z.string() }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.recipe.findUnique({ where: { id: input.id } });
+    },
+  })
+  .query("ingredients", {
+    async resolve({ ctx }) {
+      return await ctx.prisma.ingredient.findMany();
+    },
+  })
+  .mutation("add-ingredient", {
+    input: z.object({ name: z.string() }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.ingredient.create({ data: input });
+    },
+  })
+  .mutation("add-recipe", {
+    input: z.object({
+      name: z.string(),
+      instructions: z.string(),
+      image: z.string().optional(),
+    }),
+    async resolve({ ctx, input }) {
+      return await ctx.prisma.recipe.create({
+        data: input,
+      });
+    },
+  })
+  .mutation("add-ingredient-to-recipe", {
+    input: z.object({
+      ingredientId: z.string(),
+      amount: z.string(),
+      recipeId: z.string(),
+    }),
+    async resolve({ ctx, input }) {
+      await ctx.prisma.ingredientAmount.create({
+        data: input,
+      });
+    },
+  })
+  .mutation("update-recipe", {
+    input: z.object({
+      id: z.string(),
+      image: z.string().optional(),
+      instructions: z.string().optional(),
+      name: z.string().optional(),
+    }),
+    async resolve({ ctx, input }) {
+      await ctx.prisma.recipe.update({ data: input, where: { id: input.id } });
+    },
+  });
