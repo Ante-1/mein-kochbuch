740f32d02d06b89cf0dd0e1e49a82e04829078f5
Ante Schwender <ante.schwender@gmail.com>
2022-07-26 01:17:22 +0200

    add dialog backdrop and close button fix hights

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -32,29 +32,31 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <meta name="description" content="Eine Rezept-Seite mit Zutatensuche" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <nav className="text-2xl w-full bg-gray-200 py-2 px-4">
-        <span className="absolute text-3xl">
-          Mein <span className="text-blue-500">Kochbuch</span>
-        </span>
-        <ul className="flex items-center justify-center gap-16">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/search">Rezept Suchen</Link>
-          </li>
-          <button
-            onClick={openDialog}
-            className="px-3 pb-1 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105"
-          >
-            Neues Rezept anlegen
-          </button>
-        </ul>
-      </nav>
-      <main className="container mx-auto flex flex-col items-center h-screen p-4 text-gray-700">
-        {children}
-      </main>
-      <Dialog isOpen={isOpen}>
+      <div className="h-screen">
+        <nav className="text-2xl w-full bg-gray-200 py-2 px-4">
+          <span className="absolute text-3xl">
+            Mein <span className="text-blue-500">Kochbuch</span>
+          </span>
+          <ul className="flex items-center justify-center gap-16">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/search">Rezept Suchen</Link>
+            </li>
+            <button
+              onClick={openDialog}
+              className="px-3 pb-1 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105"
+            >
+              Neues Rezept anlegen
+            </button>
+          </ul>
+        </nav>
+        <main className="container mx-auto flex flex-col items-center p-4 text-gray-700">
+          {children}
+        </main>
+      </div>
+      <Dialog isOpen={isOpen} close={() => setOpen(false)}>
         <h2 className="text-2xl mb-2">Neues Rezept</h2>
         <form onSubmit={handleSubmit} className="flex flex-col gap-6">
           <label className="block mb-2 text-sm font-medium w-full">
